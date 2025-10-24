ef30fd70a952bacf34ce702d8f1045175d967273
DoGukKim <doguk1994@gmail.com>
2025-10-24 18:22:27 +0900

    feat: useIsClient custom hook 추가

---
+++ src/app/hooks/useIsClient.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const useIsClient = () => {
+  const [isClient, setIsClient] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient;
+};
+
+export default useIsClient;
